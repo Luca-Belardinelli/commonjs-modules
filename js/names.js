bc51a3634cd2a4bae7b84edd0b32ef325e89cd6a
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-31 15:51:07 +0100

    [names js]

---
+++ js/names.js
@@ -0,0 +1,25 @@
+// - names.js
+
+// 1. Crea una funzione che accetta due parametri: firstName, lastName. 
+// La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+// 2. Esporta la funzione dal file.2 
+
+
+// CREO VARIABILI
+
+const firstName = "Luca";
+const lastName = "Belardinelli";
+
+
+// CREO LA FUNZIONE
+
+function names(firstName, lastName){
+
+    return  {
+        firstName: "Luca",
+        lastName: "Belardinelli",
+    };
+}
+
+
+
